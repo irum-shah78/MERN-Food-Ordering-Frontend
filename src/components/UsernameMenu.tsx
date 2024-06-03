9ce70d660bcac4a1364b266aa6bf20f11c37919f
irum-shah78 <irams4812@gmail.com>
2024-06-03 18:27:48 +0500

    restaurant page styling

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -15,13 +15,18 @@ const UsernameMenu = () => {
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
+          <Link to="/manage-restaurant" className="font-bold hover:text-orange-500">
+            Manage Restaurant
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
           <Link to="/user-profile" className="font-bold hover:text-orange-500">
             User Profile
           </Link>
         </DropdownMenuItem>
         <Separator />
         <DropdownMenuItem>
-          <Button onClick={()=> logout()} className="flex flex-1 font-bold bg-orange-500">
+          <Button onClick={() => logout()} className="flex flex-1 font-bold bg-orange-500">
             Log Out
           </Button>
         </DropdownMenuItem>
---
+++ src/forms/manage-restaurant-form/CuisineCheckbox.tsx
@@ -0,0 +1,14 @@
+import { ControllerRenderProps, FieldValues } from "react-hook-form";
+
+type Props = {
+  cuisine: string;
+  field: ControllerRenderProps<FieldValues, "cuisines">;
+};
+
+const CuisineCheckbox = ({cuisine, field}: Props) =>{
+  return (
+    
+  )
+};
+
+export default CuisineCheckbox;
